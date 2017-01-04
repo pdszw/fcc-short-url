bca2d0dc1177e10fa502c96a620359a0fc780607
Peter Szweryn <peter.szweryn@gmail.com>
2017-01-04 12:01:40 +0100

    moved a few debug logging lines below their respective comments.

--- __old/short-url-003__WORKING__NEEDS_FURTHER_REFACTOR__20170104-1152.js
+++ __old/short-url-003__WORKING__NEEDS_FURTHER_REFACTOR__20170104-1152.js
@@ -64,12 +64,11 @@ app.get('/:input_id', function (req,res) {
     // otherwise,
     } else {
         DEBUG && console.log('short_url ' + shortURL + ' is valid');
-
         // declare mongo query to find that short_url
         var shortURLQuery = url.findOne({"short_url": shortURL}).maxTime(10000);
 
-        // try the query with a .then() (not a promise, but similar enough function for what we need).
         DEBUG && console.log('searching for short_url in db...');
+        // try the query with a .then() (not a promise, but similar enough function for what we need).
         shortURLQuery.then(function(docShort) {
             // if document found,
             if (docShort) {
@@ -95,12 +94,11 @@ app.get('/new/:input_url*', function (req,res) {
     // if newLongURL is a valid URL
     if (validURL.isUri(newLongURL)) {
         DEBUG && console.log('long input_url ' + newLongURL + ' is valid');
-
         // declare mongo query to find if that url already exists in db
         var longURLQuery = url.findOne({"original_url": newLongURL}).maxTime(10000);
 
-        // try the query (with not a promise but similar -- see above).
         DEBUG && console.log('searching for long_url in db...');
+        // try the query (with not a promise but similar -- see above).
         longURLQuery.then(function(docLong) {
             // if document found (new url already exists in db),
             if (docLong) {
@@ -112,8 +110,8 @@ app.get('/new/:input_url*', function (req,res) {
                 // declare mongo query to find largest short_url currently in db
                 var largestShortURLQuery = url.find().sort({"short_url": -1}).limit(1).maxTime(10000);
 
-                // and try the query
                 DEBUG && console.log('finding largest short_url in db...');
+                // and try the query
                 largestShortURLQuery.then(function(docLargestShort) {
                     // if document found (found largest short_url),
                     if (docLargestShort) {
